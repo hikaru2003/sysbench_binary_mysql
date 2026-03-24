54b4e6d0823bc99099ab1aaac20a07a8a7f8d3d3
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2026-03-24 09:44:55 +0900

    lua pathのついか

--- exec_script.sh
+++ exec_script.sh
@@ -2,6 +2,8 @@
 # どのディレクトリから実行しても、このスクリプトと同じ場所の test_sysbench_average.sh を使う
 cd "$(dirname "$0")" || exit 1
 
+export LUA_PATH="/users/Morisaki/sysbench_binary_mysql/share/sysbench/?.lua;/users/Morisaki/sysbench_binary_mysql/share/sysbench/?/init.lua:${LUA_PATH}"
+
 ./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=baseline tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4
 ./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=delay_0 delay=0 multiplier=50 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4
 ./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_5_delay_6 delay=6 multiplier=5 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4

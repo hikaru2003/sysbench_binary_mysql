ca1c1e26226a14742d717fa06fbddbaffe6deefc
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2026-04-02 11:41:21 +0900

    add script to collect power consumption result

---
+++ collect_power_consumption_result.sh
@@ -0,0 +1,8 @@
+python3 grep_watts.py baseline/power_run1.csv
+python3 calc_mean.py  baseline/power_run1_watts.csv
+python3 grep_watts.py delay_0/power_run1.csv
+python3 calc_mean.py  delay_0/power_run1_watts.csv
+python3 grep_watts.py multiplier_5_delay_1/power_run1.csv
+python3 calc_mean.py  multiplier_5_delay_1/power_run1_watts.csv
+python3 grep_watts.py multiplier_5_delay_6/power_run1.csv
+python3 calc_mean.py  multiplier_5_delay_6/power_run1_watts.csv

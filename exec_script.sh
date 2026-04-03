38faa6fbbb20bfc2c4febaaa52ffb278d66fb333
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2026-04-03 02:23:39 -0500

    update multiplier 100-500

--- exec_script.sh
+++ exec_script.sh
@@ -4,12 +4,15 @@ cd "$(dirname "$0")" || exit 1
 
 export LUA_PATH="/users/Morisaki/sysbench_binary_mysql/share/sysbench/?.lua;/users/Morisaki/sysbench_binary_mysql/share/sysbench/?/init.lua:${LUA_PATH}"
 
-./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=default tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
-./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=delay_0 delay=0 multiplier=50 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
-./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_1 delay=6 multiplier=1 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
-./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_2 delay=6 multiplier=2 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
-./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_4 delay=6 multiplier=4 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
-./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_8 delay=6 multiplier=8 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
-./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_16 delay=6 multiplier=16 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
-./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_32 delay=6 multiplier=32 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
-./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_100 delay=6 multiplier=100 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+#./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=default tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+#./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=delay_0 delay=0 multiplier=50 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+#./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_1 delay=6 multiplier=1 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+#./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_2 delay=6 multiplier=2 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+#./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_4 delay=6 multiplier=4 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+#./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_8 delay=6 multiplier=8 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+#./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_16 delay=6 multiplier=16 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+#./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_32 delay=6 multiplier=32 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_200 delay=6 multiplier=200 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_300 delay=6 multiplier=300 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_400 delay=6 multiplier=400 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
+./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=multiplier_500 delay=6 multiplier=500 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off

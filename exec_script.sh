3247f26018dff1f42bec3fd897d96bd07ba6b5d6
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2026-04-08 12:55:27 +0900

    update mysql.cnf

--- exec_script.sh
+++ exec_script.sh
@@ -2,7 +2,9 @@
 # どのディレクトリから実行しても、このスクリプトと同じ場所の test_sysbench_average.sh を使う
 cd "$(dirname "$0")" || exit 1
 
+# cloudlab
 export LUA_PATH="/users/Morisaki/sysbench_binary_mysql/share/sysbench/?.lua;/users/Morisaki/sysbench_binary_mysql/share/sysbench/?/init.lua:${LUA_PATH}"
+# ann server
 #export LUA_PATH="/home/morisaki/sysbench_binary_mysql/share/sysbench/?.lua;/home/morisaki/sysbench_binary_mysql/share/sysbench/?/init.lua:${LUA_PATH}"
 
 ./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=default delay=6 multiplier=50 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off

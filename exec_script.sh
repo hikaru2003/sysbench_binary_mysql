6182dd93044b675f6513983023c1fa4b4ff6e19c
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2026-04-08 16:15:27 +0900

    return prev version

--- exec_script.sh
+++ exec_script.sh
@@ -3,9 +3,9 @@
 cd "$(dirname "$0")" || exit 1
 
 # cloudlab
-#export LUA_PATH="/users/Morisaki/sysbench_binary_mysql/share/sysbench/?.lua;/users/Morisaki/sysbench_binary_mysql/share/sysbench/?/init.lua:${LUA_PATH}"
+export LUA_PATH="/users/Morisaki/sysbench_binary_mysql/share/sysbench/?.lua;/users/Morisaki/sysbench_binary_mysql/share/sysbench/?/init.lua:${LUA_PATH}"
 # ann server
-export LUA_PATH="/home/morisaki/sysbench_binary_mysql/share/sysbench/?.lua;/home/morisaki/sysbench_binary_mysql/share/sysbench/?/init.lua:${LUA_PATH}"
+#export LUA_PATH="/home/morisaki/sysbench_binary_mysql/share/sysbench/?.lua;/home/morisaki/sysbench_binary_mysql/share/sysbench/?/init.lua:${LUA_PATH}"
 
 ./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=default delay=6 multiplier=50 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
 ./test_sysbench_average.sh mysql-db=sbtest table_size=100 rand-type=pareto filename=delay_0 delay=0 multiplier=50 spin_loops=30 tables=1 threads=32 time=60 runs=10 read_threads=4 write_threads=4 histogram=off
